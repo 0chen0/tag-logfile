ba715694fdde70230879b91d50ddb772e5c6ad46
0chen0 <571480413@qq.com>
2020-08-13 02:19:20 +0800

    添加特性: 单行注释标签, 时间差计算

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 python ./main.py ./test.log > ./result.xml
 """
 import re, sys
+import datetime
 
 configure = [
 	{
@@ -16,36 +17,95 @@ configure = [
 		, "end" : r'disconnect database'
 		, "annotation" : r'操作\1数据库\2'
 	}
+	, {
+		"oneline" : r'xxxx'
+		, "annotation" : r'单行注释'
+	}
 ]
 
-if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print('Error param')
-		exit(0)
-	
-	src_file = sys.argv[1]
-	file = open(src_file, 'r')
+def getTimeInterval(begin, end):
+	pattern = re.compile(r'\[(\d\d:\d\d:\d\d)\]')
+	match = re.search(pattern, end)
+	if not match:
+		return ""
+	end_time = datetime.datetime.strptime(match.expand(r'\1'), "%H:%M:%S")
+	match = re.search(pattern, begin)
+	if not match:
+		return ""
+	begin_time = datetime.datetime.strptime(match.expand(r'\1'), "%H:%M:%S")
+	interval = (end_time - begin_time).seconds
+	return " interval=\"%ss\"" % interval
 
-	_stack = []
-	_tail = []
-	_len = 0
-	for line in file.readlines():
-		if _len > 0 and _stack[_len-1]:
-			match_end = re.search(_stack[_len-1], line)
-			if match_end:
-				sys.stdout.write("%s</L%d.%s>\n" % (line, _len, _tail[_len-1]))
-				_stack.pop(-1)
-				_tail.pop(-1)
-				_len -= 1
-				continue
-		for cfg in configure:
-			match_begin = re.search(cfg["begin"], line)
-			if match_begin:
-				pattern_end = match_begin.expand(cfg["end"])
-				tag = match_begin.expand(cfg["annotation"])
-				_stack.append(pattern_end)
-				_tail.append(tag)
-				_len += 1
-				sys.stdout.write("<L%d.%s>\n" % (_len, tag))
-				continue
-		sys.stdout.write(line)
+
+class Record():
+	def __init__(self):
+		self.end_pattern = ""
+		self.tag = ""
+		self.row = 0
+
+class LogParser():
+	def __init__(self, srcFile, attrsFunc=None):
+		self.srcFile = srcFile
+		self.attrsFunc = attrsFunc
+		self.lines = []
+		self.stack = []
+		self.depth = 0
+	
+	def parse(self, rules):
+		for row in range(len(self.lines)):
+			if self.depth > 0:
+				record = self.stack[-1]
+				match_end = re.search(record.end_pattern, self.lines[row])
+				if match_end:
+					attrs = ""
+					for attrFunc in self.attrsFunc:
+						attrs += attrFunc(self.lines[record.row], self.lines[row])
+					self.lines[record.row] = self.lines[record.row] % attrs			# 标签头设置属性
+					self.lines[row] = "%s</L%d.%s>\n" % (self.lines[row], self.depth, record.tag)	# 标签尾
+					self.stack.pop(-1)
+					self.depth -= 1
+					continue
+			for cfg in rules:
+				if "oneline" in cfg:
+					match_oneline = re.search(cfg["oneline"], self.lines[row])
+					if match_oneline:
+						self.lines[row] = "<%s/>\n%s" % (match_oneline.expand(cfg["annotation"]), self.lines[row])
+						continue
+				elif "begin" in cfg:
+					match_begin = re.search(cfg["begin"], self.lines[row])
+					if match_begin:
+						record = Record()
+						record.end_pattern = match_begin.expand(cfg["end"])		# 结束行匹配
+						record.tag = match_begin.expand(cfg["annotation"])		# xml标签名
+						record.row = row
+						self.stack.append(record)
+						self.depth += 1
+						self.lines[row] = "<L%d.%s%s>\n%s" % (self.depth, record.tag, "%s", self.lines[row])
+						continue
+
+	def readFromFile(self):
+		rfd = open(self.srcFile, 'r')
+		self.lines = rfd.readlines()
+		rfd.close()
+
+	def writeToFile(self, dstFile=None):
+		if not dstFile:
+			dstFile = "%s.xml" % self.srcFile
+		wfd = open(dstFile, 'w')
+		wfd.writelines(self.lines)
+		wfd.close()
+
+if __name__ == "__main__":
+	# if len(sys.argv) != 2:
+	# 	print('Error param')
+	# 	exit(0)	
+	# src_file = sys.argv[1]
+
+	logParser = LogParser('test.log', [getTimeInterval])
+	logParser.readFromFile()
+	logParser.parse(configure)
+	logParser.writeToFile()
+	
+
+
+
